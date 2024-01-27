d9043b56baf35f8d5de8cc19ae5679c584c90da0
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-01-28 00:33:52 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (37.59%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,38 @@
-//OPTIMISED - 1(BINARY SEARCH)
+//OPTIMISED - 1(BINARY SEARCH -> KK'S WAY)
 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        int len = nums.length;
+        int[] ans = {-1, -1};
+        ans[0] = search(nums, target, true);
+        if(ans[0] != -1){
+            ans[1] = search(nums, target, false);
+        }
+        
+        return ans;
+    }
+    
+    private int search(int[] nums, int target, boolean searchFirstIndex){
         int low = 0;
-        int high = len - 1;
+        int high = nums.length - 1;
         int index = -1;
         
-        //FIRST OCCURENCE
         while(low <= high){
             int mid = low + (high-low)/2;
             
             if(nums[mid] == target){
                 index = mid;
-                high = mid - 1;
+                if(searchFirstIndex){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
             }else if(nums[mid] < target){
                 low = mid + 1;
             }else{
                 high = mid - 1;
             }
         }
-        ans[0] = index;
         
-        //LAST OCCURENCE
-        low = 0;
-        high = len - 1;
-        index = -1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid] == target){
-                index = mid;
-                low = mid + 1;
-            }else if(nums[mid] < target){
-                low = mid + 1;
-            }else if(nums[mid] > target){
-                high = mid - 1;
-            }
-        }
-        ans[1] = index;
-        
-        return ans;
+        return index;
     }
 }
