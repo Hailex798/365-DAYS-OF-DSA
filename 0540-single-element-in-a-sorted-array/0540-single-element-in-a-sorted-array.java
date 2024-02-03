16c195c9644efe9ef149db2faf8ec17a80242616
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-02-04 03:38:49 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (31.32%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,18 @@
+//OPTIMISED - 1.3(BINARY SEARCH)
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        
+        while(low < high){
+            int mid = low + (high-low)/2;
+            
+            if((mid&1) == 1) mid--;
+            if(nums[mid] != nums[mid+1]) high = mid;
+            else low = mid + 2;
+        }
+        return nums[low];
+    }
+}
