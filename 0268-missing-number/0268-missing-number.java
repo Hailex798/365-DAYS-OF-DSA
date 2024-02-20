bfeb10b9a1f222ea1a3e216ee21c06fee1a6d32c
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-02-20 09:40:47 +0530

    Time: 5 ms (31.08%), Space: 45.1 MB (61.43%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+//BRUTE FORCE - 1(SORTING)
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        Arrays.sort(nums);
+        
+        for(int i=0 ; i<len ; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        
+        return len;
+    }
+}
