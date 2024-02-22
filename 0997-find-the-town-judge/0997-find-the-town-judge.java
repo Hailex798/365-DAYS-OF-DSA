689a98fd823ce06c16a01950cbf36015e10298c1
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-02-22 12:12:01 +0530

    Time: 6 ms (30.97%), Space: 52.6 MB (80.43%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,25 +1,29 @@
-//BRUTE FORCE - 1(DOUBLE ITERATION: ONE PASS)
+//OPTIMISED - 2(HASHSET)
 
 class Solution {
     public int findJudge(int n, int[][] trust) {
+        Set<Integer> set = new HashSet<>();
         
+        //ADDING ALL THE N PEOPLE
         for(int i=1 ; i<=n ; i++){
-            boolean flag = true;
-            int count = 0;
-            for(int j=0 ; j<trust.length ; j++){
-                if(i == trust[j][0]){
-                    flag = false;
-                    break;
-                }
-                if(i == trust[j][1]){
-                    count++;
-                }
-            }
-            if(flag == true && count == n-1){
-                return i;
-            }
+            set.add(i);
         }
         
-        return -1;
+        //REMOVING EVERYBODY WHO TRUSTS ANYONE
+        for(int[] i: trust){
+            set.remove(i[0]);
+        }
+        
+        Iterator<Integer> iterator = set.iterator();
+        if(set.isEmpty()) return -1;
+        
+        //CHECKING CONDN. 2 --> IF EVERYBODY TRUST THE POTENTIAL JUDGE
+        int val = iterator.next();
+        
+        for(int[] i: trust){
+            if(i[1] == val) n--;
+        }
+        
+        return n == 1 ? val : -1;
     }
 }
