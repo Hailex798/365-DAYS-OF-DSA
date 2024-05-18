cdf5c85aee6fef32667921edea5eadf44403955b
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-05-18 15:25:24 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (81.70%) - LeetHub

--- 0104-maximum-depth-of-span-classwiseone-analysis-result-wiseone-analysis-result-entitybinary-tree-span/0104-maximum-depth-of-span-classwiseone-analysis-result-wiseone-analysis-result-entitybinary-tree-span.java
+++ 0104-maximum-depth-of-span-classwiseone-analysis-result-wiseone-analysis-result-entitybinary-tree-span/0104-maximum-depth-of-span-classwiseone-analysis-result-wiseone-analysis-result-entitybinary-tree-span.java
@@ -20,9 +20,9 @@ class Solution {
         if(root == null){
             return 0;
         }
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        int lheight = maxDepth(root.left);
+        int rheight = maxDepth(root.right);
         
-        return Math.max(left, right) + 1;
+        return lheight > rheight ? lheight + 1 : rheight + 1;
     }
 }
