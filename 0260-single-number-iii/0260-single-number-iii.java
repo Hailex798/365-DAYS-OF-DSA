5de173faba43655abdda8f63e2a8751fba9e69dc
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-05-31 22:09:11 +0530

    Time: 7 ms (18.56%), Space: 45.2 MB (95.04%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,19 +1,20 @@
-//BF - 1(SORTING)
+//BF - 2(FREQUENCY: HASHMAP)
 
 class Solution {
     public int[] singleNumber(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[2];
-        int len = nums.length;
         int x = 0;
         
-        for(int i=0 ; i<len ; i++){
-            if(i == len-1){
+        //MAPPING FREQUENCY
+        for(int i: nums){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        
+        //CHECKING NO. W 1 FREQUENCY
+        for(int i=0 ; i<nums.length ; i++){
+            if(map.get(nums[i]) == 1){
                 ans[x++] = nums[i];
-            }else if(nums[i] != nums[i+1]){
-                ans[x++] = nums[i];
-            }else{
-                i += 1;
             }
         }
         
