771c8471a3f115becc1904fe0e200d00405debd7
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-06-28 00:11:03 +0530

    Time: 0 ms (100.00%), Space: 67.1 MB (48.59%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,21 +1,11 @@
-//OPTIMISED - 2(SELF)
+//OPTIMISED - 3(SELF)
 
 class Solution {
     public int findCenter(int[][] edges) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        //MAPPED FREQUENCIES
-        for(int[] ed: edges){
-            int first = map.getOrDefault(ed[0], 0);
-            if(first != 0) return ed[0];
-            int second = map.getOrDefault(ed[1], 0);
-            if(second != 0) return ed[1];
-            map.put(ed[0], first+1);
-            map.put(ed[1], second+1);
-        }
-        
-        if(map.get(edges[0][0]) == edges.length) return edges[0][0];
-        if(map.get(edges[0][1]) == edges.length) return edges[0][1];
+        if(edges[1][0] == edges[0][0] || edges[1][0] == edges[0][1]) return edges[1][0];
+        if(edges[1][1] == edges[0][0] || edges[1][1] == edges[0][1]) return edges[1][1];
         
         return 1;
     }
