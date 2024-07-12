36911c6b4bcff477d4f2d1c8b9ff7c93b27b75fd
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-07-12 23:53:18 +0530

    Time: 1 ms (99.98%), Space: 45.6 MB (44.42%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int len = nums.length;
+        
+        int goal = len - 1;
+        
+        for(int i=len-1 ; i>=0 ; i--){
+            if(i + nums[i] >= goal) goal = i;
+        }
+        
+        return goal == 0 ? true : false;
+    }
+}
