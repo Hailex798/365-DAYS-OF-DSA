604d20ca147c57fe52374a85c35fbba8cff16c6f
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-07-19 00:49:14 +0530

    Time: 2 ms (70.20%), Space: 62.2 MB (8.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,16 @@
+//OPTIMISED - 1(MINIMA + MAXIMA)
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int minima = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        int minima = prices[0];
+        int maxima = Integer.MIN_VALUE;
         
         for(int i=0 ; i<prices.length ; i++){
-            if(prices[i] < minima){
-                minima = prices[i];
-            }
-            max = Math.max(max, prices[i] - minima);
+            if(prices[i] < minima) minima = prices[i];
+            
+            maxima = Math.max(maxima, prices[i] - minima);
         }
         
-        return max;
+        return maxima;
     }
 }
