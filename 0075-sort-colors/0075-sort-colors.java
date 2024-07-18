76850453f2f79abd4a906a946d27a30f9379e54b
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-07-19 00:54:26 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (7.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,27 +1,16 @@
-//BF - (COUNTING)
-
 class Solution {
     public void sortColors(int[] nums) {
-        int zero = 0;
-        int ones = 0;
+        int zero = 0; int one = 0;
         
-        //COUNTING 0'S & 1'S
         for(int i: nums){
             if(i == 0) zero++;
-            if(i == 1) ones++;
+            if(i == 1) one++;
         }
         
-        //SORTING IN-PLACE BY FREQUENCY
         for(int i=0 ; i<nums.length ; i++){
-            if(zero > 0){
-                nums[i] = 0;
-                zero--;
-            }else if(ones > 0){
-                nums[i] = 1;
-                ones--;
-            }else{
-                nums[i] = 2;
-            }
+            if(zero-- > 0) nums[i] = 0;
+            else if(one-- > 0) nums[i] = 1;
+            else nums[i] = 2;
         }
         
         return ;
