771c72f2056c6718b5031396c6e0ed38daa9c36e
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-08-01 14:28:45 +0530

    Time: 18 ms (6.23%), Space: 43.1 MB (10.46%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -1,14 +1,14 @@
+//OPTIMISED - 1(BASIC)
+
 class Solution {
     public int countSeniors(String[] details) {
         int older = 0;
-        String age = "";
         
         for(String i: details){
-            age += i.charAt(11);
-            age += i.charAt(12);
-            if(Integer.valueOf(age) > 60) older++;
-            
-            age = "";
+            int x = Integer.valueOf(i.charAt(11) - '0');
+            x = x*10 + Integer.valueOf(i.charAt(12) - '0');
+            System.out.println(x);
+            if(x > 60) older++;
         }
         
         return older;
