6a4237e3cece34298d8fe0d00bc23d319db5af0c
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-08-07 16:47:23 +0530

    Time: 28 ms (41.21%), Space: 56.3 MB (37.04%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -5,12 +5,13 @@ class Solution {
         return nums;
     }
     
-    //MERGE SORT -> MAIN IMPLEMENTATION
     public int[] mergeSort(int[] arr){
         int len = arr.length;
+        
         if(len == 1){
             return arr;
         }
+        
         int mid = len/2;
         
         int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
@@ -19,12 +20,11 @@ class Solution {
         return merge(left, right);
     }
     
-    public static int[] merge(int[] left, int[] right){
+    public int[] merge(int[] left, int[] right){
         int[] mix = new int[left.length + right.length];
-        
-        int x = 0;
         int i = 0;
         int j = 0;
+        int x = 0;
         
         while(i < left.length && j < right.length){
             if(left[i] < right[j]){
