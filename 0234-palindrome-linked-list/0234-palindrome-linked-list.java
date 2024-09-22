d02061445e78f9951088684d54cd95fecebd43ac
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-09-22 17:52:38 +0530

    Time: 1316 ms (5.35%), Space: 58.1 MB (79.15%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,31 +1,19 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//BF - 1(LL -> NO.)
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        String s = "";
+        String dig = "";
         
         while(head != null){
-            s += Integer.toString(head.val);
+            dig += head.val;
             head = head.next;
         }
         
-        return pal(s);
-    }
-    
-    public boolean pal(String s){
-        int low = 0;
-        int high = s.length() - 1;
+        int i=0;
+        int j=dig.length()-1;
         
-        while(low < high){
-            if(s.charAt(low++) != s.charAt(high--)) return false;
+        while(i<j){
+            if(dig.charAt(i++) != dig.charAt(j--)) return false;
         }
         
         return true;
