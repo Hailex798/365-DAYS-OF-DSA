324e9d84c05e740b11a15d810599c912c4d94f01
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-11-01 01:33:40 +0530

    Time: 17 ms (5.36%), Space: 44.3 MB (14.09%) - LeetHub

--- 1863-sum-of-all-subset-span-classwiseone-analysis-result-wiseone-analysis-result-entityxor-span-totals/1863-sum-of-all-subset-span-classwiseone-analysis-result-wiseone-analysis-result-entityxor-span-totals.java
+++ 1863-sum-of-all-subset-span-classwiseone-analysis-result-wiseone-analysis-result-entityxor-span-totals/1863-sum-of-all-subset-span-classwiseone-analysis-result-wiseone-analysis-result-entityxor-span-totals.java
@@ -1,16 +1,35 @@
-//OPTIMISED - 2(BACKTRACKING)
-
 class Solution {
     public int subsetXORSum(int[] nums) {
-        return xor(nums, 0, 0);
+        List<List<Integer>> subsets = new ArrayList<>();
+        // Generate all of the subsets
+        generateSubsets(nums, 0, new ArrayList<>(), subsets);
+
+        // Compute the XOR total for each subset and add to the result
+        int result = 0;
+        for (List<Integer> subset : subsets) {
+            int subsetXORTotal = 0;
+            for (int num : subset) {
+                subsetXORTotal ^= num;
+            }
+            result += subsetXORTotal;
+        }
+        return result;
     }
-    public int xor(int[] nums, int idx, int xor){
-        if(idx == nums.length) return xor;
-        
-        int withX = xor(nums, idx+1, xor ^ nums[idx]);
-        
-        int withoutX = xor(nums, idx+1, xor);
-        
-        return withX + withoutX;
+
+    private void generateSubsets(int[] nums, int index, List<Integer> subset, List<List<Integer>> subsets) {
+        // Base case: index reached end of nums
+        // Add the current subset to subsets
+        if (index == nums.length) {
+            subsets.add(new ArrayList<>(subset));
+            return;
+        }
+
+        // Generate subsets with nums[i]
+        subset.add(nums[index]);
+        generateSubsets(nums, index + 1, subset, subsets);
+        subset.remove(subset.size() - 1);
+
+        // Generate subsets without nums[i]
+        generateSubsets(nums, index + 1, subset, subsets);
     }
 }
