7964367a215eef4101f125ddf08b8d6271d3dd42
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2024-12-02 18:52:00 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (27.01%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.java
@@ -7,8 +7,6 @@ class Solution {
         int len = searchWord.length();
         
         for(int i=0 ; i<arr.length ; i++){
-            if(arr[i].length() < len) continue;
-            
             if(arr[i].startsWith(searchWord)) return i+1;
         }
         
