65befec7ffd76b55d23696a490ec7ac357fe6abf
Kshitij Singh Bisht <kshitij.singh.bisht.174sis@gmail.com>
2025-01-09 00:50:17 +0530

    Time: 2 ms (100%), Space: 42.5 MB (44.44%) - LeetHub

--- 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
+++ 3042-count-prefix-and-suffix-pairs-i/3042-count-prefix-and-suffix-pairs-i.java
@@ -1,39 +1,13 @@
-//BRUTE FORCE - 1(CONTEST: TWO FUNCTIONS)
-
 class Solution {
     public int countPrefixSuffixPairs(String[] words) {
-        int len = words.length;
-        int both = 0;
-        
-        //CHECK FOR ALL POSSIBLE PAIRS OF SUFFIX AND PREFIX EQUALITY
-        for(int i=0 ; i<len-1 ; i++){
-            for(int j=i+1 ; j<len ; j++){
-                if((prefix(words[i], words[j])) && (sufix(words[i], words[j]))){
-                    both++;
-                }
+        int index = 0;
+
+        for(int i=0 ; i<words.length ; i++){
+            for(int j=i+1 ; j<words.length ; j++){
+                if(words[j].startsWith(words[i]) && words[j].endsWith(words[i])) index++;
             }
         }
-        
-        return both;
-    }
-    
-    public boolean prefix(String w1, String w2){
-        int len = w1.length();
-        if(len > w2.length()) return false;
-        
-        for(int i=0 ; i<len ; i++){
-            if(w1.charAt(i) != w2.charAt(i)) return false;
-        }
-        return true;
-    }
-    
-    public boolean sufix(String w1, String w2){
-        int len1 = w1.length();
-        int len2 = w2.length();
-        
-        for(int i=len2-len1, j=0 ; i<len2 ; i++, j++){
-            if(w1.charAt(j) != w2.charAt(i)) return false;
-        }
-        return true;
+
+        return index;
     }
 }
